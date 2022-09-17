0268cd8f49177e7e13017402dfdf2abc15da48d1
Shantonu Acharjee <shantonuacharjee@gmail.com>
2022-09-17 20:14:55 +0600

    Auto Door Lock Ultrasonic Done

---
+++ Auto_Door_Open/Auto_Door_Open.ino
@@ -0,0 +1,45 @@
+#include <Servo.h>
+Servo AutoDoorServo;
+
+
+int trigPinA =A0;
+int echoPinA =A1; //Should Be analog
+int distanceA;
+long durationA;
+int led = 13;
+
+void setup(){
+ pinMode(trigPinA,OUTPUT);
+ pinMode(echoPinA,INPUT);
+ pinMode(led, OUTPUT);
+
+ AutoDoorServo.attach(9);
+ Serial.begin(9600);
+  
+}
+
+
+
+void loop() {
+
+digitalWrite(trigPinA,LOW);
+delayMicroseconds(2);
+digitalWrite(trigPinA,HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPinA,LOW);
+durationA=pulseIn(echoPinA,HIGH);
+distanceA = durationA*0.034/2;
+
+
+if(distanceA <= 15){
+  AutoDoorServo.write(100);
+  delay(1000);
+}
+
+else{
+  AutoDoorServo.write(0);
+  delay(15);
+}
+
+Serial.println(distanceA);
+}
